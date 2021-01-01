48cbfa0f86f1c0e69de0e23a31cea871a27aaabc
Krzysztof Olipra <contact@krzysztofolipra.com>
2021-01-01 17:45:21 +0100

    feat: swagger api docs.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Movies')
+    .setDescription('Movies REST API')
+    .setVersion('1.0')
+    .addTag('movies')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
--- backend/src/user/user.controller.spec.ts
+++ backend/src/user/user.controller.spec.ts
@@ -1,19 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
-import { UserService } from './user.service';
-import { UserRepository } from './user.repository';
 
 describe('UserController', () => {
   let controller: UserController;
-  let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [UserController, UserService, UserRepository],
+      controllers: [UserController],
     }).compile();
 
     controller = module.get(UserController);
-    service = module.get(UserService);
   });
 
   it('should be defined', () => {
