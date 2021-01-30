6191fd4e22c2b897a6c2dc2d5562013ee89690e7
Krzysztof Olipra <contact@krzysztofolipra.com>
2021-01-31 00:30:43 +0100

    ci: client sonar setup.

--- client/jest.config.js
+++ client/jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
   transformIgnorePatterns: [
     'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*)',
   ],
+  reporters: ['default', 'jest-sonar'],
   projects: [
     {
       preset: 'jest-expo/ios',
