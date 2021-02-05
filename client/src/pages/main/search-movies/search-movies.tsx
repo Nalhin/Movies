820249c8881d9e4d0d0b30ae7567b3adeee85d59
Krzysztof Olipra <contact@krzysztofolipra.com>
2021-02-06 00:57:59 +0100

    feat: search movies page.

--- client/src/pages/main/search-movies/search-movies.tsx
+++ client/src/pages/main/search-movies/search-movies.tsx
@@ -1,8 +1,54 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import { FlatList, SafeAreaView } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { useInfiniteQuery } from 'react-query';
+import { getSearchMoviesPage } from '../../../core/api/movie/movie.api';
+import MovieCard from '../../../shared/components/movie-card/movie-card';
+import { ROOT_ROUTES } from '../../root.routes';
+import { useDebounce } from 'use-debounce';
+import { Input } from 'react-native-elements';
 
 const SearchMovies = () => {
-  return <View />;
+  const navigation = useNavigation();
+  const [search, setSearch] = useState('');
+  const [debouncedSearch] = useDebounce(search, 500);
+  const { data, fetchNextPage } = useInfiniteQuery(
+    ['projects', debouncedSearch],
+    async ({ pageParam = 1 }) => {
+      return getSearchMoviesPage(pageParam, debouncedSearch).then(
+        (resp) => resp.data,
+      );
+    },
+    {
+      getPreviousPageParam: (firstPage) =>
+        firstPage.page > 1 ? firstPage.page - 1 : false,
+      getNextPageParam: (lastPage) =>
+        lastPage.hasNextPage ? lastPage.page + 1 : false,
+      enabled: !!debouncedSearch,
+    },
+  );
+
+  return (
+    <SafeAreaView>
+      <Input onChangeText={(text) => setSearch(text)} value={search} />
+      <FlatList
+        data={data?.pages.flatMap((page) => page.data)}
+        renderItem={({ item }) => (
+          <MovieCard
+            title={item.title}
+            posterPath={item.posterPath}
+            onPress={() =>
+              navigation.navigate(ROOT_ROUTES.MOVIE_DETAILS, {
+                movieId: item.id,
+              })
+            }
+          />
+        )}
+        keyExtractor={(item) => String(item.id)}
+        onEndReached={() => fetchNextPage()}
+      />
+    </SafeAreaView>
+  );
 };
 
 export default SearchMovies;
